1b413ccf5dae07699f8ed2e5a4e0e4ff9a205df7
YanaYafim <tablet.ejv@gmail.com>
2024-04-18 13:45:40 +0200

    commit zad1

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const PORT = 3000;
+const { getCars, getCarInformation, getCarAge } = require('./cars');
+const { getHTMLDocumentStart, getHTMLDocumentEnd } = require('./htmlGenerator');
+
+const server = http.createServer((req, res) => {
+    const cars = getCars();
+    console.log(cars);
+    res.setHeader('Content-Type', 'text/html');
+    const htmlStart = getHTMLDocumentStart();
+    const htmlEnd = getHTMLDocumentEnd();
+    res.write(htmlStart);
+    res.write(`<body>`);
+    res.write(`<p>${getCarInformation(cars[0].id)}</p>`);
+    res.write(`<p>${getCarAge(cars[0].id)}</p>`);
+    res.write(`</body>`);
+    res.write(htmlEnd);
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}.`);
+});
---
+++ cars.js
@@ -0,0 +1,37 @@
+const cars = [
+    { id: 1, make: "Toyota", model: "Yaris", year: 2001, color: "white" },
+    { id: 2, make: "Honda", model: "Bird", year: 2009, color: "blue" },
+    { id: 3, make: "Ford", model: "First", year: 2015, color: "red" },
+    { id: 4, make: "BMW", model: "Winner", year: 2021, color: "black" },
+    { id: 5, make: "Toyota", model: "Golf", year: 2019, color: "blue" }
+];
+
+function getCars() {
+    return cars;
+}
+
+function getCarInformation(id) {
+const car = getCars().find(car => car.id === id);
+    if (car) {
+        return `Make: ${car.make}, Model: ${car.model}, Year: ${car.year}, Color: ${car.color}.`;
+    } else {
+        return "Car doesn't exist";
+    }
+}
+
+function getCarAge(id) {
+const car = getCars().find(car => car.id === id);
+    if (car) {
+        const currentYear = new Date().getFullYear();
+        const carAge = currentYear - car.year;
+        return `Car is ${carAge} years old.`;
+    } else {
+        return "Car doesn't exist";
+    }
+}
+console.log(getCarInformation(2));
+module.exports = {
+    getCars,
+    getCarInformation,
+    getCarAge
+};
